4a3effa0f68282edc9d11c11c1d73be5e051a5b7
xiomara huamani <xiomara.huamani.c@gmail.com>
2024-08-05 17:32:02 -0500

    merch2

--- pages/_app.js
+++ pages/_app.js
@@ -15,7 +15,7 @@ export default function App({ Component, pageProps }) {
     <Fragment>
       <JeenaHead />
       {loading && <Preloader />}
-      <Component {...pageProps} />
+     
     </Fragment>
   );
 }
