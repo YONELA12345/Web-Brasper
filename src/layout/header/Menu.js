ec81db4db3ff9d1ecc99cc0bce82a7aacf3cf9f2
xiomara huamani <xiomara.huamani.c@gmail.com>
2024-09-04 15:03:52 -0500

    navbar modify dolares and reales

--- src/layout/header/Menu.js
+++ src/layout/header/Menu.js
@@ -1,33 +1,19 @@
 import Link from "next/link";
-import { useState, useEffect, Fragment } from "react";
+import { useState, Fragment } from "react";
 import { Accordion } from "react-bootstrap";
 import { useLocale } from "../../../context/LocaleContext"; // Asegúrate de importar el contexto de idioma
 
 const Menu = () => {
-  const { locale, changeLocale, t } = useLocale();
-  const [selectedLocale, setSelectedLocale] = useState(locale);
-
-  useEffect(() => {
-    changeLocale(selectedLocale);
-  }, [selectedLocale, changeLocale]);
-
   return (
     <Fragment>
-      <DeskTopMenu
-        selectedLocale={selectedLocale}
-        setSelectedLocale={setSelectedLocale}
-        t={t}
-      />
-      <MobileMenu
-        selectedLocale={selectedLocale}
-        setSelectedLocale={setSelectedLocale}
-        t={t}
-      />
+      <DeskTopMenu />
+      <MobileMenu />
     </Fragment>
   );
 };
 
-const MobileMenu = ({ selectedLocale, setSelectedLocale, t }) => {
+const MobileMenu = () => {
+  const { locale, changeLocale, t } = useLocale(); // Usa el contexto de idioma para obtener traducciones
   const [activeMenu, setActiveMenu] = useState(null);
 
   const active = (value) => setActiveMenu(value === activeMenu ? null : value),
@@ -69,30 +55,31 @@ const MobileMenu = ({ selectedLocale, setSelectedLocale, t }) => {
                 {t.navbar.home} {/* Traducción de "Inicio" */}
               </Link>
             </li>
+
+            <li className="dropdown">
+              <Link href="/about">{t.navbar.about}</Link>
+            </li>
+
+            <li className="dropdown">
+              <Link href="#">{t.navbar.howItWorks}</Link>
+            </li>
             <li className="dropdown">
               <Link href="" onClick={() => active("services")}>
                 {t.navbar.services} {/* Traducción de "Servicios" */}
               </Link>
             </li>
-            <li className="dropdown">
-              <Link href="/about">{t.navbar.about}</Link>
-            </li>
-            <li className="dropdown">
-              <Link href="#">{t.navbar.howItWorks}</Link>
-            </li>
-
+            {/* Selector de idioma */}
             <li className="dropdown">
               <select
-                value={selectedLocale}
-                onChange={(e) => setSelectedLocale(e.target.value)}
+                value={locale}
+                onChange={(e) => changeLocale(e.target.value)}
                 style={{
-                  background: "#000", // Fondo negro
-                  border: "1px solid #444", // Borde oscuro
-                  color: "#fff", // Texto blanco
+                  background: "#000000",
+                  border: "none",
+                  color: "#fff",
                   padding: "5px",
                   borderRadius: "4px",
                 }}
-                className="bg-black text-white"
               >
                 <option value="es">Español</option>
                 <option value="en">English</option>
@@ -106,7 +93,9 @@ const MobileMenu = ({ selectedLocale, setSelectedLocale, t }) => {
   );
 };
 
-const DeskTopMenu = ({ selectedLocale, setSelectedLocale, t }) => {
+const DeskTopMenu = () => {
+  const { locale, changeLocale, t } = useLocale(); // Usa el contexto de idioma para obtener traducciones
+
   return (
     <nav className="main-menu navbar-expand-lg desktop-menu ">
       <div className="navbar-header">
@@ -133,34 +122,35 @@ const DeskTopMenu = ({ selectedLocale, setSelectedLocale, t }) => {
       <div className="navbar-collapse collapse clearfix">
         <ul className="navigation clearfix">
           <li className="dropdown">
-            <Link href="/">{t.navbar.home}</Link>
+            <Link href="/">{t.navbar.home}</Link> {/* Traducción de "Inicio" */}
+          </li>
+
+          <li className="dropdown">
+            <Link href="/about">{t.navbar.about}</Link>{" "}
+            {/* Traducción de "¿Quiénes somos?" */}
+          </li>
+
+          <li className="dropdown">
+            <Link href="#">{t.navbar.howItWorks}</Link>{" "}
+            {/* Traducción de "¿Cómo funciona?" */}
           </li>
           <li className="dropdown">
             <Link href="#">{t.navbar.services}</Link>{" "}
+            {/* Traducción de "Servicios" */}
           </li>
-          <li className="dropdown">
-            <Link href="/about">{t.navbar.about}</Link>{" "}
-          </li>
-          <li className="dropdown">
-            <Link href="#">{t.navbar.howItWorks}</Link>{" "}
-          </li>
-          <li className="dropdown">
-            <Link legacyBehavior href="https://rb.gy/vjpce3" target="_blank">
-              {t.navbar.contactUs}
-            </Link>
-          </li>
-          <li className="dropdown">
+
+          {/* Selector de idioma */}
+          <li className="dropdown" style={{ color: "#fff" }}>
             <select
-              value={selectedLocale}
-              onChange={(e) => setSelectedLocale(e.target.value)}
+              value={locale}
+              onChange={(e) => changeLocale(e.target.value)}
               style={{
-                background: "#000", // Fondo negro
-                border: "1px solid #444", // Borde oscuro
-                color: "#fff", // Texto blanco
+                background: "#0f1425",
+                border: "none",
+                color: "#fff",
                 padding: "5px",
                 borderRadius: "4px",
               }}
-              className="bg-black text-white"
             >
               <option value="es">Español</option>
               <option value="en">English</option>
