252d6eaa052179c59c94462b1da72724b57df4fb
NIls <alberthxen@gmail.com>
2024-10-11 18:32:27 -0500

    responsive de navbar y sidevar

--- pages/admin/components/navbar/index.js
+++ pages/admin/components/navbar/index.js
@@ -1,101 +1,132 @@
-import { useRouter } from "next/router"; // Para redirigir al usuario
+import React from "react";
 
 const Navbar = () => {
-  const router = useRouter();
-
-  // Función para manejar el cierre de sesión
-  const handleLogout = () => {
-    // Eliminar el token de localStorage
-    localStorage.removeItem("token");
-    localStorage.removeItem("user");
-    
-    router.push("/login");
-  };
-
   return (
-    <nav className="navbar top-bar navbar-light border-bottom py-0 py-xl-3">
-      <div className="container-fluid p-0">
-        <div className="d-flex align-items-center w-100">
-          {/* Logo START */}
-          <div className="d-flex align-items-center d-xl-none">
-            <a className="navbar-brand" href="index.html">
-              <img
-                className="light-mode-item navbar-brand-item h-30px"
-                src="assets/images/logo-mobile.svg"
-                alt="Logo"
-              />
-              <img
-                className="dark-mode-item navbar-brand-item h-30px"
-                src="assets/images/logo-mobile-light.svg"
-                alt="Logo"
-              />
-            </a>
-          </div>
-          {/* Logo END */}
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        {/* Logo START */}
 
-          <div className="ms-xl-auto">
-            <ul className="navbar-nav flex-row align-items-center">
+        {/* Logo END */}
+
+        {/* Toggler for sidebar START */}
+        <button
+          className="navbar-toggler d-xl-none"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarContent"
+          aria-controls="navbarContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-animation">
+            <span></span>
+            <span></span>
+            <span></span>
+          </span>
+        </button>
+        {/* Toggler for sidebar END */}
+
+        {/* Collapsible content START */}
+        <div className="collapse navbar-collapse d-xl-none" id="navbarContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#home">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#about">
+                About
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#services">
+                Services
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="ms-xl-auto">
+          <ul className="navbar-nav flex-row align-items-center">
+            {/* Notification dropdown START */}
+            {/*     <li className="nav-item ms-2 ms-md-3 dropdown">
+              <a
+                className="btn btn-light btn-round mb-0"
+                href="#"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+                data-bs-auto-close="outside"
+              >
+                <i className="bi bi-bell fa-fw"></i>
+              </a>
+              <span className="notif-badge animation-blink"></span>
+
+           
+            </li> */}
+            {/* Notification dropdown END */}
+
+            {/* Profile dropdown START */}
+            <li className="nav-item ms-2 ms-md-3 dropdown">
+              <a
+                className="avatar avatar-sm p-0"
+                href="#"
+                id="profileDropdown"
+                role="button"
+                data-bs-auto-close="outside"
+                data-bs-display="static"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <img
+                  className="avatar-img rounded-circle"
+                  src="assets/images/logos/logo_principal.png"
+                  alt="avatar"
+                />
+              </a>
+
               {/* Profile dropdown START */}
-              <li className="nav-item ms-2 ms-md-3 dropdown">
-                <a
-                  className="avatar avatar-sm p-0"
-                  href="#"
-                  id="profileDropdown"
-                  role="button"
-                  data-bs-auto-close="outside"
-                  data-bs-display="static"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  <img
-                    className="avatar-img rounded-circle"
-                    src="assets/images/avatar/01.jpg"
-                    alt="avatar"
-                  />
-                </a>
-
-                {/* Profile dropdown START */}
-                <ul
-                  className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3"
-                  aria-labelledby="profileDropdown"
-                >
-                  <li className="px-3">
-                    <div className="d-flex align-items-center">
-                      <div className="avatar me-3 mb-3">
-                        <img
-                          className="avatar-img rounded-circle shadow"
-                          src="assets/images/avatar/01.jpg"
-                          alt="avatar"
-                        />
-                      </div>
-                      <div>
-                        <a className="h6 mt-2 mt-sm-0" href="#">
-                          Lori Ferguson
-                        </a>
-                        <p className="small m-0">example@gmail.com</p>
-                      </div>
+              <ul
+                className="dropdown-menu dropdown-animation dropdown-menu-end shadow pt-3"
+                aria-labelledby="profileDropdown"
+              >
+                {/*  <li className="px-3">
+                  <div className="d-flex align-items-center">
+                    <div className="avatar me-3 mb-3">
+                      <img
+                        className="avatar-img rounded-circle shadow"
+                        src="assets/images/avatar/01.jpg"
+                        alt="avatar"
+                      />
                     </div>
-                  </li>
-                  <li>
-                    {" "}
-                    <hr className="dropdown-divider" />
-                  </li>
-
-                  <li>
-                    <a
-                      className="dropdown-item bg-danger-soft-hover"
-                      href="#"
-                      onClick={handleLogout} // Llama a la función para cerrar sesión
-                    >
-                      <i className="bi bi-power fa-fw me-2"></i>Cerrar sesión
-                    </a>
-                  </li>
-                </ul>
-                {/* Profile dropdown END */}
-              </li>
+                    <div>
+                      <a className="h6 mt-2 mt-sm-0" href="#">
+                        Lori Ferguson
+                      </a>
+                      <p className="small m-0">example@gmail.com</p>
+                    </div>
+                  </div>
+                </li> */}
+                {/*   <li>
+                  {" "}
+                  <hr className="dropdown-divider" />
+                </li>
+ */}
+                <li>
+                  <a className="dropdown-item bg-danger-soft-hover" href="#">
+                    <i className="bi bi-power fa-fw me-2"></i>Cerrar sesion
+                  </a>
+                </li>
+              </ul>
               {/* Profile dropdown END */}
-            </ul>
-          </div>
+            </li>
+            {/* Profile dropdown END */}
+          </ul>
         </div>
       </div>
     </nav>
--- pages/admin/components/sidbar/index.js
+++ pages/admin/components/sidbar/index.js
@@ -1,152 +1,153 @@
-'use client';
-import Link from 'next/link';
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/router';
+"use client";
+import Link from "next/link";
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const OffcanvasSidebar = () => {
   const router = useRouter();
 
   useEffect(() => {
-    // Importar Bootstrap JS solo en el cliente
-    import('bootstrap/dist/js/bootstrap.bundle.min.js');
+    // Este código solo se ejecutará en el cliente, no en el servidor
+    import("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
 
+  // Función para determinar si un enlace está activo
   const isActive = (path) => router.pathname === path;
 
   return (
-    <nav className="navbar navbar-expand-xl navbar-dark bg-dark">
-      <div className="container-fluid">
-        {/* Botón de menú para dispositivos móviles */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasSidebar"
-          aria-controls="offcanvasSidebar"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
+    <nav className="navbar sidebar navbar-expand-xl navbar-dark bg-dark">
+      {/* Navbar brand for xl START */}
+      <div className="d-flex align-items-center">
+        <Link className="navbar-brand" href="/">
+          <img
+            src="/assets/images/logos/logo_completo.png"
+            alt="Logo"
+            title="Logo"
+          />
+        </Link>
+      </div>
+      {/* Navbar brand for xl END */}
 
-        {/* Offcanvas para dispositivos móviles y sidebar para pantallas grandes */}
-        <div
-          className="offcanvas offcanvas-start bg-dark custom-offcanvas"
-          id="offcanvasSidebar"
-          tabIndex="-1"
-          data-bs-backdrop="true"
-        >
-          <div className="offcanvas-header">
-            {/* Logo */}
-            <Link className="navbar-brand" href="/">
-              <img
-                src="/assets/images/logos/logo_completo.png"
-                alt="Logo"
-                title="Logo"
-                style={{ height: '40px' }}
-              />
-            </Link>
-            <button
-              type="button"
-              className="btn-close btn-close-white"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div className="offcanvas-body d-flex flex-column p-3">
-            <ul className="navbar-nav flex-column" id="navbar-sidebar">
-              <li className="nav-item">
-                <Link
-                  href="/admin"
-                  className={`nav-link ${isActive('/admin') ? 'active' : ''}`}
-                >
-                  <i className="fas fa-house fa-fw me-2"></i>Dashboard
-                </Link>
-              </li>
-              <li className="nav-item ms-2 my-2 text-white">Vistas</li>
-              <li className="nav-item">
-                <Link
-                  href="/admin/users"
-                  className={`nav-link ${isActive('/admin/users') ? 'active' : ''}`}
-                >
-                  <i className="far fa-user-alt fa-fw me-2"></i>Usuarios
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link
-                  href="/admin/tasa"
-                  className={`nav-link ${isActive('/admin/tasa') ? 'active' : ''}`}
-                >
-                  <i className="fas fa-percentage fa-fw me-2"></i>Tipo Cambio
-                </Link>
-              </li>
-              <li className="nav-item">
-                <a
-                  className="nav-link"
-                  data-bs-toggle="collapse"
-                  href="#collapsepage"
-                  role="button"
-                  aria-expanded="false"
-                  aria-controls="collapsepage"
-                >
-                  <i className="fas fa-money-bill fa-fw me-2"></i>Rangos
-                </a>
-                <ul className="nav collapse flex-column" id="collapsepage">
-                  <li className="nav-item">
-                    <Link
-                      href="/admin/solreal"
-                      className={`nav-link ${isActive('/admin/solreal') ? 'active' : ''}`}
-                    >
-                      Sol a Real
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      href="/admin/realsol"
-                      className={`nav-link ${isActive('/admin/realsol') ? 'active' : ''}`}
-                    >
-                      Real a Sol
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      href="/admin/dolarreal"
-                      className={`nav-link ${isActive('/admin/dolarreal') ? 'active' : ''}`}
-                    >
-                      Dólar a Real
-                    </Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link
-                      href="/admin/realdolar"
-                      className={`nav-link ${isActive('/admin/realdolar') ? 'active' : ''}`}
-                    >
-                      Real a Dólar
-                    </Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link
-                  href="/admin/settings"
-                  className={`nav-link ${isActive('/admin/settings') ? 'active' : ''}`}
-                >
-                  <i className="fas fa-user-cog fa-fw me-2"></i>Admin Settings
-                </Link>
-              </li>
-            </ul>
-          </div>
+      <div
+        className="offcanvas offcanvas-start flex-row custom-scrollbar h-100"
+        data-bs-backdrop="true"
+        tabIndex="-1"
+        id="offcanvasSidebar"
+      >
+        <div className="offcanvas-body sidebar-content d-flex flex-column bg-dark">
+          {/* Sidebar menu START */}
+          <ul className="navbar-nav flex-column" id="navbar-sidebar">
+            {/* Menu item 1 */}
+            <li className="nav-item">
+              <Link
+                href="/admin"
+                className={`nav-link ${isActive("/admin") ? "active" : ""}`}
+              >
+                <i className="fas fa-house fa-fw me-2"></i>Dashboard
+              </Link>
+            </li>
+
+            {/* Title */}
+            <li className="nav-item ms-2 my-2 ">Vistas</li>
+
+            {/* Menu item 2 */}
+            <li className="nav-item">
+              <Link
+                href="/admin/users"
+                className={`nav-link ${
+                  isActive("/admin/users") ? "active" : ""
+                }`}
+              >
+                <i className="far fa-user-alt fa-fw me-2"></i>Usuarios
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                data-bs-toggle="collapse"
+                href="#collapsepage"
+                role="button"
+                aria-expanded="false"
+                aria-controls="collapsepage"
+              >
+                <i className="fas fa-money-bill fa-fw me-2"></i>Rangos
+              </a>
+              {/* Submenu */}
+              <ul
+                className="nav collapse flex-column"
+                id="collapsepage"
+                data-bs-parent="#navbar-sidebar"
+              >
+                <li className="nav-item">
+                  <Link
+                    href="/admin/solreal"
+                    className={`nav-link ${
+                      isActive("/admin/solreal") ? "active" : ""
+                    }`}
+                  >
+                    Sol a Real
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    href="/admin/realsol"
+                    className={`nav-link ${
+                      isActive("/admin/realsol") ? "active" : ""
+                    }`}
+                  >
+                    Real a Sol
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    href="/admin/dolarreal"
+                    className={`nav-link ${
+                      isActive("/admin/dolarreal") ? "active" : ""
+                    }`}
+                  >
+                    Dolar a Real
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link
+                    href="/admin/realdolar"
+                    className={`nav-link ${
+                      isActive("/admin/realdolar") ? "active" : ""
+                    }`}
+                  >
+                    Real a Dolar
+                  </Link>
+                </li>
+              </ul>
+            </li>
+
+            {/* Menu item 6 */}
+            {/* <li className="nav-item">
+              <Link
+                href="/admin/earnings"
+                className={`nav-link ${
+                  isActive("/admin/earnings") ? "active" : ""
+                }`}
+              >
+                <i className="far fa-chart-bar fa-fw me-2"></i>Earnings
+              </Link>
+            </li> */}
+
+            {/* Menu item 7 */}
+            <li className="nav-item">
+              <Link
+                href="/admin/settings"
+                className={`nav-link ${
+                  isActive("/admin/settings") ? "active" : ""
+                }`}
+              >
+                <i className="fas fa-user-cog fa-fw me-2"></i>Admin Settings
+              </Link>
+            </li>
+          </ul>
+          {/* Sidebar menu end */}
         </div>
       </div>
-      <style jsx>{`
-        .custom-offcanvas {
-          width: 250px; /* Ajusta el ancho del offcanvas */
-        }
-
-        @media (max-width: 576px) {
-          .custom-offcanvas {
-            width: 80%; /* Menor ancho en dispositivos móviles */
-          }
-        }
-      `}</style>
     </nav>
   );
 };
