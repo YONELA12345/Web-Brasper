94f82e16e789297a02090f010f888616df6408b1
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-10-28 09:18:55 -0500

    cambio al 3% en las comiciones

--- src/data/commissionRates.js
+++ src/data/commissionRates.js
@@ -8,12 +8,12 @@ const commissionRates = {
     { min: 5000, max: 100000, rate: 0.03 }
   ],
   "USD-BRL": [
-    { min: 100, max: 199, rate: 0.035 },
-    { min: 200, max: 299, rate: 0.035 },
-    { min: 300, max: 499, rate: 0.035 },
-    { min: 500, max: 999, rate: 0.035 },
-    { min: 1000, max: 4999, rate: 0.035 },
-    { min: 5000, max: 100000, rate: 0.035 }
+    { min: 100, max: 199, rate: 0.030 },
+    { min: 200, max: 299, rate: 0.030 },
+    { min: 300, max: 499, rate: 0.030 },
+    { min: 500, max: 999, rate: 0.030 },
+    { min: 1000, max: 4999, rate: 0.030 },
+    { min: 5000, max: 100000, rate: 0.030 }
   ],
   "BRL-PEN": [
     { min: 100, max: 199, rate: 0.045 },
