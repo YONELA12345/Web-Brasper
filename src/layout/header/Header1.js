3d21656451cee679122eae75628b4bfe4d6b50f2
XiomaraHuamani <xiomara.huamani.c@gmail.com>
2024-11-29 14:24:47 -0500

    cambios del navbar

--- src/layout/header/Header1.js
+++ src/layout/header/Header1.js
@@ -7,87 +7,17 @@ const Header1 = () => {
 
   return (
     <header className="main-header header-one menu-white">
-      {/* <div className="header-top-wrap bgc-header1">
-        <div className="container">
-          <div className="header-top mb-n4">
-            <ul>
-              <li>
-                <i className="far fa-envelope" />{" "}
-                <a
-                  href="mailto:brasper@braspertransferencias.com"
-                  style={{ color: "#fff" }}
-                >
-                  brasper@braspertransferencias.com
-                </a>
-              </li>
-              <li>
-                <i className="far fa-phone" />{" "}
-                <a href="callto:+51966991933" style={{ color: "#fff" }}>
-                  +51 966991933
-                </a>
-              </li>
-              
-              <li>
-                <div className="social-style-one">
-                  <a
-                    href="https://www.facebook.com/BrasPerTransferencias"
-                    target="_blank"
-                    style={{ color: "#fff" }}
-                  >
-                    <i className="fab fa-facebook-f" />
-                  </a>
-                  <a
-                    href="https://www.tiktok.com/@braspertransferencias"
-                    target="_blank"
-                    style={{ color: "#fff" }}
-                  >
-                    <i className="fab fa-tiktok" />
-                  </a>
-                  <a
-                    href="https://www.instagram.com/brasper.transferencias"
-                    target="_blank"
-                    style={{ color: "#fff" }}
-                  >
-                    <i className="fab fa-instagram" />
-                  </a>
-                  <a
-                    href="https://www.linkedin.com/in/brasper-transferencias-7a7b7b311/"
-                    target="_blank"
-                    style={{ color: "#fff" }}
-                  >
-                    <i className="fab fa-linkedin-in" />
-                  </a>
-                </div>
-              </li>
-              <li></li>
-            </ul>
-          </div>
-        </div>
-      </div> */}
-      {/*Header-Upper*/}
       <div className="header-upper bgc-black">
         <div className="container clearfix">
           <div className="header-inner rel d-flex align-items-center">
-            {/* <div className="logo-outer">
-              <div className="logo">
-                <Link legacyBehavior href="/">
-                  <img
-                    src="assets/images/logos/logo_principal.png"
-                    alt="Logo"
-                    title="Logo"
-                  />
-                </Link>
-              </div>
-            </div> */}
             <img
-              src="assets/images/logos/logo_completo.png"
+              src="assets/images/logos/logo_blanco.png"
               alt="Logo"
               title="Logo"
+              className="logo"
             />
             <div className="nav-outer mx-auto clearfix">
-              {/* Main Menu */}
               <Menu />
-              {/* Main Menu End */}
             </div>
             {/* <div className="menu-btns">
               <Link legacyBehavior href="login">
